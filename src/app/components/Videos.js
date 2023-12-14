eb413ba4772136167c3308e97d6550a315f9803f
ericjiji3 <eric.ji3333@gmail.com>
2023-12-13 19:33:05 -0600

    dknfdk

--- src/app/components/Videos.js
+++ src/app/components/Videos.js
@@ -1,10 +1,20 @@
 import Image from 'next/image'
-import styles from './videos.css'
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import './videos.css'
 
 export default function Videos() {
   return (
-    <div>
-      videos
+    <div className="videosContainer">
+            <div className="videoContainer">
+              <iframe src="https://www.youtube.com/embed/FJXv3mLnBuo" title="Interview Prank (Drake Edition)" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+            </div>
+            <div className="videoContainer">
+              <iframe src="https://www.youtube.com/embed/BIMy2Df9rNA" title="Interview Prank (Toilet Edition) #shorts #prank #interview" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+            </div>
+            <div className="videoContainer">
+              <iframe src="https://www.youtube.com/embed/uhugRCqST8k" title="I got NO DICK mannnn #shorts #dance" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+            </div>
+      
     </div>
   )
 }
